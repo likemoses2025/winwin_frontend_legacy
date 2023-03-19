56428429a80a047528fd39e678b77c933b221d3f
likemoses2025 <mose233@gmail.com>
2023-03-19 19:13:00 +0900

    Return Confirm

--- src/screens/returns/ReturnConfirm.js
+++ src/screens/returns/ReturnConfirm.js
@@ -1,11 +1,17 @@
+import {useNavigation} from '@react-navigation/native';
 import React, {useCallback, useEffect, useState} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import gunnySackNumber from '../../../assets/datas/GunnySackNumber';
 import yearMonth from '../../../assets/datas/YearMonth';
+import SelectDropdown from 'react-native-select-dropdown';
 
-const ReturnConfirm = ({route}) => {
+const ReturnConfirm = ({route, navigation}) => {
   const filteredInputData = route.params?.inputData;
   const [filterData, setFilterData] = useState('');
+
+  const navigate = useNavigation();
+
+  const countries = ['Egypt', 'Canada', 'Australia', 'Ireland'];
 
   // 품목별 합계
 
@@ -17,13 +23,26 @@ const ReturnConfirm = ({route}) => {
 
   return (
     <View style={{marginTop: 100}}>
-      <TouchableOpacity onPress={navigatiton.Back()}>
+      <TouchableOpacity onPress={() => navigate.goBack()}>
         <Text>뒤로가기</Text>
       </TouchableOpacity>
       <Text>ReturnConfirm</Text>
-      {filteredInputData.map(item => (
-        <Text>{item.product_no}</Text>
-      ))}
+      <SelectDropdown
+        data={countries}
+        onSelect={(selectedItem, index) => {
+          console.log(selectedItem, index);
+        }}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          // text represented after item is selected
+          // if data array is an array of objects then return selectedItem.property to render after item is selected
+          return selectedItem;
+        }}
+        rowTextForSelection={(item, index) => {
+          // text represented for each item in dropdown
+          // if data array is an array of objects then return item.property to represent item in dropdown
+          return item;
+        }}
+      />
     </View>
   );
 };
